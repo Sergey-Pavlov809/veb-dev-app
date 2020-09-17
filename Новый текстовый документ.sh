8ce05e671c2bfa37f57ba3c3e813de92a51afc61
Sergey Pavlov <sergey-pavlov2610@mail.ru>
2020-09-18 00:29:01 +0300

    Commit Veb Dev

---
+++ Новый текстовый документ.sh
@@ -0,0 +1,12 @@
+    
+#!/bin/sh
+
+# Author : Pavlov Sergey Variant 5
+# Script : 
+# лучше через UID, чем через whoami, так как имя пользователя запустившего процесс может быть алиасом или обрезано  
+echo Wake up $(whoami)
+echo The Matrix has you...
+echo Your UID = $UID
+ps aux > processes.txt
+grep $UID processes.txt > myprocesses.txt
+read
